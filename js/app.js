ab47bb9ff7f7440a220f7a8583399ffed5f026e2
Dominique Patino <DPatin2@wgu.com>
2020-10-11 15:02:36 -0700

    Added lightbox, description overlay, and responsiveness

--- js/app.js
+++ js/app.js
@@ -26,5 +26,45 @@ $(document).ready(function() {
         }
         // Stop link behavior
         return false;
-    })
-})
+    });
+
+    // MouseEnter overlay
+    $('ul#gallery li').on('mouseenter', function() {
+        // Get data attribute values
+        var title = $(this).children().data('title');
+        var desc = $(this).children().data('desc');
+
+        // Validation
+        if(desc == null) {
+            desc = 'Click to Enlarge';
+        }
+
+        if(title == null) {
+            title = '';
+        }
+
+        // Create overlay div
+        $(this).append('<div class="overlay"></div>');
+
+        // Get the overlay div
+        var overlay = $(this).children('.overlay');
+
+        // Add html to overlay
+        overlay.html('<h3>'+title+'</h3><p>'+desc+'</p>');
+
+        // Fade in overlay
+        overlay.fadeIn(800);
+    });
+
+    // MouseLeave overlay
+    $('ul#gallery li').on('mouseleave', function() {
+                // Create overlay div
+                $(this).append('<div class="overlay"></div>');
+
+                // Get the overlay div
+                var overlay = $(this).children('.overlay');
+
+                // Fade out overlay
+                overlay.fadeOut(500);
+    });
+});
